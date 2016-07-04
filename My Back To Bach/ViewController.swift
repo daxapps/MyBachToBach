8ea6482ddf044edd806adcc9d334d86e42092d5e
Jason Crawford <daxapps777@gmail.com>
2016-07-04 08:48:10 -0500

    mostly debugged, it works

--- My Back To Bach/ViewController.swift
+++ My Back To Bach/ViewController.swift
@@ -7,19 +7,81 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
 
+    @IBAction func play(sender: AnyObject) {
+        
+        player.play()
+        
+    }
+    @IBOutlet var volumnSlider: UISlider!
+    
+    @IBAction func adjustVolume(sender: AnyObject) {
+        
+        player.volume = volumnSlider.value
+        
+    }
+    @IBOutlet var scrubSlider: UISlider!
+    
+    @IBAction func scrub(sender: AnyObject) {
+        
+        player.currentTime = NSTimeInterval(scrubSlider.value)
+        
+    }
+    @IBAction func pause(sender: AnyObject) {
+        
+        player.pause()
+        
+    }
+    @IBAction func stop(sender: AnyObject) {
+        
+        player.pause()
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bach-bwv904-breemer", ofType: "mp3")!))
+            
+        } catch {
+            
+            // It didn't work!
+            
+        }
+        
+    }
+    
+    func updateScrubSlider() {
+        
+        scrubSlider.value = Float(player.currentTime)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bach-bwv904-breemer", ofType: "mp3")!))
+            
+            scrubSlider.maximumValue = Float(player.duration)
+            
+        } catch {
+            
+            // It didn't work!
+            
+        }
+        
+        _ = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateScrubSlider"), userInfo: nil, repeats: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
